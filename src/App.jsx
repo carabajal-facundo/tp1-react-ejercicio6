32212f1ae59e86b2f1c577f51f2d50e58d35ebe3
carabajal-facundo <facarabajal@hotmail.com>
2023-05-28 18:42:19 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
